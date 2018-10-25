eb332da4922e8859d268335bb5b7fee47af25bbe
bill <williamandrewbain@gmail.com>
2018-10-25 14:41:55 -0600

    Minisculed port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3666
+const port = process.env.port || 3666
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const messagesRoutes = require('./routes/messages')
